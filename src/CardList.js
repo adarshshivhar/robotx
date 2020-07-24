646d6d0366f3590dde9800413797b92cb88d6c62
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-07-24 19:13:48 +0530

    Another Way of doing

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    const cardArray = robots.map((user, i) => {
-        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
-    })
     return (
         <div>
-            {cardArray}
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card key={i}
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            email={robots[i].email}
+                        />
+                    );
+                })
+            }
         </div>
     );
 }
