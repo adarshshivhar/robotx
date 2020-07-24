002fb5a7bd79356d1e303a8fd6654b3f5e5a610c
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-07-24 23:20:40 +0530

    Adding Seach Feature

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    );
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value })
+    }
+
+    render() {
+        const filterRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filterRobots} />
+            </div>
+        );
+    }
 }
 
 export default App;
