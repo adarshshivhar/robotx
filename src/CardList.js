d9c09e4d07b3e820a46c2b728c03ab73733ee458
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-07-24 19:05:52 +0530

    Add CardList

---
+++ src/CardList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    const cardArray = robots.map((user, i) => {
+        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+    })
+    return (
+        <div>
+            {cardArray}
+        </div>
+    );
+}
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <CardList robots={robots}/>
+  ,
   document.getElementById('root')
 );
 
