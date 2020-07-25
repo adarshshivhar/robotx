536905df972efbb2b9ed2eb8557086d3ade27119
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-07-25 18:44:19 +0530

    Css Added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
-
+import './App.css';
 
 class App extends React.Component {
     constructor() {
@@ -23,7 +23,7 @@ class App extends React.Component {
         })
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filterRobots} />
             </div>
