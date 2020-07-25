9e875f8de96f6ce1a0c5cffa9763cbe335935ef1
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-07-25 18:48:14 +0530

    Getting Data using REST api Concept

--- src/App.js
+++ src/App.js
@@ -8,10 +8,22 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+          return response.json();
+        })
+        .then(users => {
+          this.setState({robots: users})
+        });
+    }
+
 
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value })
