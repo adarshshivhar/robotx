89445823105ef705baa014a060fb35ca37c51c4b
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-07-25 20:34:28 +0530

    Center Loding....

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 
 class App extends React.Component {
@@ -32,16 +32,20 @@ class App extends React.Component {
                     robot.email.toLowerCase().includes(this.state.searchfield.toLowerCase()));
         })
         if(this.state.robots.length===0){
-          return <h1>Loading...</h1>
+          return(
+            <div className='h1Center'>
+              <h1>Loading...</h1>
+            </div>
+          );
         }else{
-          return (
-              <div className='tc'>
-                  <h1 className='f1'>RoboFriends</h1>
-                  <SearchBox searchChange={this.onSearchChange}/>
-                  <Scroll>
-                    <CardList robots={filterRobots} />
-                  </Scroll>
-              </div>
+           return (
+               <div className='tc'>
+                   <h1 className='f1'>RoboFriends</h1>
+                   <SearchBox searchChange={this.onSearchChange}/>
+                   <Scroll>
+                     <CardList robots={filterRobots} />
+                   </Scroll>
+               </div>
           );
         }
 
